d227145fc6859015ca081b4593d6141106642d31
Sean <seansnowwhite@aol.com>
2019-12-06 00:45:14 +0100

    feat(merge): parse input of intervals

--- main.go
+++ main.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/Schneereich/merge-intervals/interval"
 )
 
 func main() {
-	intervals := []interval.Interval{
-		{15, 20},
-		{2, 6},
-		{17, 24},
+	if len(os.Args) < 2 {
+		intervals := []interval.Interval{
+			{15, 20},
+			{2, 6},
+			{17, 24},
+		}
+		log.Println("No intervals given with the execution of the program. Using default values:", intervals)
+		result := interval.Merge(intervals)
+		fmt.Println(result)
+		return
+	}
+
+	// read commandline input and concatenate
+	args := strings.TrimSpace(strings.Join(os.Args[1:],""))
+	log.Println("The following intervals are read from the input:", args)
+
+	// parse interval pairs of the commandline input
+	object, _ := regexp.Compile(`(\[(\d+),(\d+)\])`)
+	matches := object.FindAllStringSubmatch(args, -1)
+	var intervals []interval.Interval
+	for _, match := range matches {
+		start, _ := strconv.Atoi(match[2])
+		end, _ := strconv.Atoi(match[3])
+		intervals = append(intervals, interval.Interval{Start: start, End: end})
 	}
 
 	result := interval.Merge(intervals)
 	fmt.Println(result)
+	return
 }
